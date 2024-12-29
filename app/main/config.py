19f31a0aa3c089e4957100e055175d029c336d34
Retr0413 <153156670+Retr0413@users.noreply.github.com>
2024-12-29 19:16:46 +0900

    フロントエンドに機構とマークをついかした。

--- app/main/config.py
+++ app/main/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     SECRET_KEY = 'SecretKey'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@db:3306/flaskdb"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@db:3306/flaskdb?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
