5125731c74540781883166bcde50604e7d7c28b5
Retr$ <153156670+Retr0413@users.noreply.github.com>
2025-01-12 03:24:27 +0900

    バックエンドと起動方式を.envを使用して、環境変数から持ってくるようにした。

--- app/main/config.py
+++ app/main/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = 'SecretKey'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@db:3306/flaskdb?charset=utf8mb4"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
